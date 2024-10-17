47720f799ededa62b6542173b92622db9601a64c
Davy <90331454+Davytjuhh@users.noreply.github.com>
2024-10-17 21:44:08 +0200

    yeet

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-#import basic packages
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import numpy as np
@@ -9,50 +8,41 @@ import datetime
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
 app.config['DEBUG'] = True  # Enable debug mode
 
-# Construct the absolute path to the JSON file
 json_file_path = os.path.join(os.path.dirname(__file__), 'WineDataset_Complete.json')
 
-# Read the JSON file into a DataFrame
 df = pd.read_json(json_file_path)
 
-# Convert the DataFrame to a list of dictionaries
 wines = df.to_dict(orient='records')
 
-# Add an index-based ID to each wine
 for index, wine in enumerate(wines):
-    wine['id'] = index  # Use lowercase 'id'
+    wine['id'] = index  
 
-# Function to extract unique values out the json for the filters on wines.html
 def get_unique_values(data, key):
-    unique_values = set()  # Use a set to avoid duplicates
+    unique_values = set()  
     for wine in data:
         values = wine.get(key)
         if values:
-            if isinstance(values, list):  # If it's a list, add all items to the set
+            if isinstance(values, list):  
                 unique_values.update(values)
             else:
-                unique_values.add(values)  # If it's a string, add directly
+                unique_values.add(values)  
     return sorted(unique_values)
 
 @app.route('/')
 def home():
     # Seed of the random 3 wines (based on datetime)
     np.random.seed(int(datetime.datetime.now().strftime('%Y%m%d')))
-    # Randomly choose 3 wines from the dataset 
     random_wines = np.random.choice(wines, 3, replace=False)
     random_wines = [{'id': wine['id'], 'Title': wine['Title'], 'Image': wine['image_source'] } for wine in random_wines]
     print(random_wines)
     return render_template('index.html', random_wines=random_wines)
 
-# define the about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 @app.route('/wines', methods=['GET'], endpoint='wines')
 def wine_list():
-
-    # GET filters
     sort = request.args.get('sort')
     search = request.args.get('search')
     lead = request.args.get('lead')
@@ -88,46 +78,31 @@ def wine_list():
     unique_characteristics = get_unique_values(wines, 'Characteristics')
     unique_per_bottle_case_each = get_unique_values(wines, 'Per bottle / case / each')
     unique_appellations = get_unique_values(wines, 'Appellations')
-
-    # Split styles for wines that have multiple styles separated by ' & '
-    # unique_styles = sorted({style.strip() for styles in unique_styles for style in styles.split(' & ')})
-
-    # Remove vintages that contain double years (e.g., '2020/2021')
     unique_vintages = [year for year in unique_vintages if '/' not in year]
-
-    # Split wine filters for wines with multiple filters separated by ', '
     unique_secondary_grapes = sorted({grape.strip() for varieties in unique_secondary_grapes for grape in varieties.split(', ')})
     unique_characteristics = sorted({characteristic.strip() for characteristics in unique_characteristics for characteristic in characteristics.split(', ')})
 
-    # Additional filters (sliders that are nice to have for the future)
-    price_min = request.args.get('price_min', 0, type=float)  # Minimum price filter
-    price_max = request.args.get('price_max', 1000, type=float)  # Maximum price filter
-    abv_min = request.args.get('abv_min', 0, type=float)  # Minimum ABV filter
-    abv_max = request.args.get('abv_max', 100, type=float)  # Maximum ABV filter
+    price_min = request.args.get('price_min', 0, type=float)  
+    price_max = request.args.get('price_max', 1000, type=float) 
+    abv_min = request.args.get('abv_min', 0, type=float)  
+    abv_max = request.args.get('abv_max', 100, type=float)  
 
     # Filter the wines based on selected filters
     filtered_wines = wines
+    pairing_filter = request.args.get('pairing')  
 
-    pairing_filter = request.args.get('pairing')  # Get the selected pairing from the query parameters
-
-    if pairing_filter:  # If a pairing is selected
-        filtered_wines = [
-            wine for wine in wines 
-            if pairing_filter in wine.get('best_pairing', [])
-        ]
+    if pairing_filter:  
+        filtered_wines = [wine for wine in wines if pairing_filter in wine.get('best_pairing', [])]
 
     for key, value in filters.items():  # Key for example country, grape. Value for example France, Chardonnay.
         if value:
-            # Split the input value separated by ', ' for multiple filter options (e.g., "France, Italy")
             values = [v.strip() for v in value.split(',')]
-
             # If filtering on 'Secondary Grape Varieties' or 'Characteristics' or 'Style' (which are lists)
             if key in ['Secondary Grape Varieties', 'Characteristics', 'Style']:
                 filtered_wines = [
                     wine for wine in filtered_wines
                     if any(v in (wine.get(key) if isinstance(wine.get(key), list) else [wine.get(key)]) for v in values)
                 ]
-
             # For standard single-value fields like 'Country', 'Region', 'Grape', etc.
             else:
                 filtered_wines = [
@@ -135,7 +110,7 @@ def wine_list():
                     if str(wine.get(key, '')).strip() == value  # Standard exact match for non-list fields
                 ]
 
-    # Apply slidebar filters (price, ABV)
+    # Apply filters (price, ABV)
     filtered_wines = [wine for wine in filtered_wines if price_min <= wine.get('Price') <= price_max]
     filtered_wines = [wine for wine in filtered_wines if abv_min <= wine.get('ABV') <= abv_max]
 
@@ -163,9 +138,9 @@ def wine_list():
         # Sort by Name
         filtered_wines.sort(key=lambda x: x['Title'], reverse=(sort == 'z-a'))
 
-    # Pagination parameters
+    # Pagination
     page = request.args.get('page', 1, type=int)
-    per_page = 12  # Number of wines to display per page
+    per_page = 12 
     total_wines = len(filtered_wines)
     total_pages = math.ceil(total_wines / per_page)
 
@@ -176,17 +151,16 @@ def wine_list():
 
     # Generate page numbers
     page_numbers = []
-    if total_pages <= 5:  # Show all page numbers if there are 5 or fewer
+    if total_pages <= 5:  
         page_numbers = list(range(1, total_pages + 1))
     else:
-        if page < 4:  # If we are on the first 3 pages
+        if page < 4: 
             page_numbers = list(range(1, 5)) + ['...'] + [total_pages]
-        elif page > total_pages - 3:  # If we are on the last 3 pages
+        elif page > total_pages - 3:  
             page_numbers = [1] + ['...'] + list(range(total_pages - 3, total_pages + 1))
-        else:  # If we are in the middle
+        else:  
             page_numbers = [1] + ['...'] + list(range(page - 1, page + 2)) + ['...'] + [total_pages]
 
-    # Pass all the data needed to build the webpage
     return render_template(
         'wines.html',
         path=request.path,
@@ -214,8 +188,7 @@ def wine_list():
 
 @app.route('/wine/<int:wine_id>')
 def wine_detail(wine_id):
-    # Logic to fetch the wine detail from wines based on wine_id
-    wine = wines[wine_id]  # This assumes wine_id corresponds to the index
+    wine = wines[wine_id]  
     return render_template('wine_detail.html', wine=wine)
 
 @app.route('/guide')
